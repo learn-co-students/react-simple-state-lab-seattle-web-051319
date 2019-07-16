70a641ba846da6bd7138dcdd7a66be0271764756
ndnguyen07 <github email address>
2019-07-16 16:22:46 -0700

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            color: this.props.value
+        }
+    }
+
+    swapClick = () => {this.setState({color: '#333'})}
+
+    render() {
+        return (
+            <div className='cell' style={{backgroundColor: this.state.color}}
+            onClick = {this.swapClick}>
+            </div>
+        ) 
+    }
+
+}
